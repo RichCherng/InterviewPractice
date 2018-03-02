42320b4395fe6b1a6e6af725a95c7624d49690c6
Rich Cherngchaosil <richcherng@gmail.com>
2018-03-01 16:16:51 -0800

    Update checkTwoBST.java

--- src/checkTwoBST.java
+++ src/checkTwoBST.java
@@ -18,3 +18,25 @@ class Solution {
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
+
+
+// Refactored
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true; // both null
+        if (p == null || q == null) return false; // one of them is null
+        if (p.val == q.val) return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return false;
+        
+        
+    }
+}
